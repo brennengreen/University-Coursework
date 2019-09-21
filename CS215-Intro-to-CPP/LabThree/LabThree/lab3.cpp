9ec1fd3d1b80e0cedcfe27447ee6e2ee7a427f21
Brennen Green <brennengreen@outlook.com>
2019-09-21 10:50:07 -0400

    some kinda update

--- CS215-Intro-to-CPP/LabThree/LabThree/lab3.cpp
+++ CS215-Intro-to-CPP/LabThree/LabThree/lab3.cpp
@@ -3,7 +3,8 @@
 //--------------------------------------------------------------------------
 // Author: Brennen Green
 // Date: 09/17/2019
-// Description: A C++ Program that does arithmetic on two integers
+// Description: A C++ Program that displays a box of selected size to the user
+// filled with characters of a selected type
 //--------------------------------------------------------------------------
 #include <iostream>
 #include <iomanip>
@@ -27,13 +28,14 @@ int main() {
 	std::cout << "-           Box Maker           -" << std::endl;
 	std::cout << "-        By: Brennen Green      -" << std::endl;
 	std::cout << "---------------------------------" << std::endl;
-
+	// Display the possible selections to the user
 	std::cout << "Box Characters:" << std::endl;
 	std::cout << "S - Star" << std::endl;
 	std::cout << "D - Dollar" << std::endl;
 	std::cout << "B - Backslash" << std::endl;
 	std::cout << std::endl;
 
+	// Until the user gives a valid selection for the type of box keep asking them
 	char user_char_selection = ' ';
 	while (user_char_selection != 'D' && user_char_selection != 'S' && user_char_selection != 'B') {
 		std::cout << "Enter your selection: ";
@@ -45,6 +47,7 @@ int main() {
 	}
 	std::cout << std::endl;
 
+	// Until the user gives a valid selection for the size of box keep asking them
 	int user_size_selection = 0;
 	while (user_size_selection < 3 || user_size_selection > 7) {
 		std::cout << "Enter size of box <3-7>: ";
@@ -55,6 +58,7 @@ int main() {
 	}
 	std::cout << std::endl;
 
+	// Define the actual character used to outline the box
 	char box_character;
 	if (user_char_selection == 'S') {
 		box_character = '*';
