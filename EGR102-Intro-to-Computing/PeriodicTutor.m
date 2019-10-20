d4e41290344233b0d13c85e5b6eb058dcf1f9484
Brennen Green <brennengreen@outlook.com>
2019-10-20 09:09:02 -0400

    egr102 proj

--- EGR102-Intro-to-Computing/PeriodicTutor.m
+++ EGR102-Intro-to-Computing/PeriodicTutor.m
@@ -1,21 +1,29 @@
-function [numCorrectSymbol, numCorrectAtomic] = PeriodicTutor (atomicNumber, symbolGuess, atomicGuess, numCorrectSymbol, numCorrectAtomic)
-    [~, ~, data] = xlsread("periodicTable.xlsx");
-    elementNames = data(:, 1);
-    elementSymbols = data(:, 2);
-    eleName = elementNames{atomicNumber};
-    eleSymbol = elementSymbols{atomicNumber};
-    
-    fprintf("%s, %s\n",eleName, eleSymbol);
-    if (symbolGuess == eleSymbol)
-        fprintf("Congratulations\n");
-        numCorrectSymbol = numCorrectSymbol + 1;
-    else
-        fprintf("Sorry! The correct symbol is: %s\n", eleSymbol);
-    end
-    if (atomicGuess == atomicNumber)
-        fprintf("Congratulations\n");
-        numCorrectAtomic = numCorrectAtomic + 1;
-    else
-        fprintf("Sorry! The correct atomic number is: %s\n", atomicNumber);
-    end
+[~, ~, data] = xlsread("periodicTable.xlsx");
+elementNames = data(:, 1);
+elementSymbols = data(:, 2);
+[numElements, ~] = size(elementNames);
+
+numToReview = input("How many elements do you want to review: ");
+for i = 1:numToReview
+    atomicNumber = randi(numElements);
+    disp(atomicNumber);
+end
+
+eleName = elementNames{atomicNumber};
+eleSymbol = elementSymbols{atomicNumber};
+
+
+
+fprintf("%s, %s\n",eleName, eleSymbol);
+if (symbolGuess == eleSymbol)
+    fprintf("Congratulations\n");
+    numCorrectSymbol = numCorrectSymbol + 1;
+else
+    fprintf("Sorry! The correct symbol is: %s\n", eleSymbol);
+end
+if (atomicGuess == atomicNumber)
+    fprintf("Congratulations\n");
+    numCorrectAtomic = numCorrectAtomic + 1;
+else
+    fprintf("Sorry! The correct atomic number is: %s\n", atomicNumber);
 end
