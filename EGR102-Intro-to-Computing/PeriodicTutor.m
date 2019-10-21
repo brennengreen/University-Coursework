f14180901c94b026718c1e6426d346a6b9db3eb7
Brennen Green <brennengreen@outlook.com>
2019-10-21 11:13:08 -0400

    t

--- EGR102-Intro-to-Computing/PeriodicTutor.m
+++ EGR102-Intro-to-Computing/PeriodicTutor.m
@@ -3,14 +3,43 @@ elementNames = data(:, 1);
 elementSymbols = data(:, 2);
 [numElements, ~] = size(elementNames);
 
+
+disp("+---------------------------------+")
+disp("-         PERIODIC TUTOR          -")
+disp("-          BRENNEN GREEN          -")
+disp("+---------------------------------+")
+
 numToReview = input("How many elements do you want to review: ");
+reviewedElements = zeros(numToReview);
+numCorrectSymbols = 0;
+numCorrectNumbers = 0;
 for i = 1:numToReview
     atomicNumber = randi(numElements);
-    disp(atomicNumber);
+    eleName = elementNames{atomicNumber};
+    eleSymbol = elementSymbols{atomicNumber};
+    reviewedElements(i) = atomicNumber;
+    
+    fprintf("Current Element: %s\n", eleName);
+    
+    symbolGuess = input("Element Symbol: ");
+    if (symbolGuess == eleSymbol)
+        fprintf("Congratulations\n");
+        numCorrectSymbol = numCorrectSymbol + 1;
+    else
+        fprintf("Sorry! The correct symbol is: %s\n", eleSymbol);
+    end
+    
+    atomicGuess = input("Element's Atomic Number: ");
+    if (atomicGuess == atomicNumber)
+        fprintf("Congratulations\n");
+        numCorrectAtomic = numCorrectAtomic + 1;
+    else
+        fprintf("Sorry! The correct atomic number is: %s\n", atomicNumber);
+    end
+    pause(0.5);
 end
 
-eleName = elementNames{atomicNumber};
-eleSymbol = elementSymbols{atomicNumber};
+
 
 
 
