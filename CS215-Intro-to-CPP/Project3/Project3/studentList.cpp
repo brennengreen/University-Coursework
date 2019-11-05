046bcc16c021eead5fda7e7e74e8eb8318e84e1d
Brennen Green <brennengreen@outlook.com>
2019-11-05 12:45:29 -0500

    Lots of Stuff | CS215Proj3 Student List Header

--- CS215-Intro-to-CPP/Project3/Project3/studentList.cpp
+++ CS215-Intro-to-CPP/Project3/Project3/studentList.cpp
@@ -1,5 +1,29 @@
 #include "studentList.h"
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
 
 studentList::studentList() {
 
+}
+
+void studentList::print()
+{
+}
+
+student studentList::search(string id)
+{
+	return student();
+}
+
+student studentList::search(int idx)
+{
+	return student();
+}
+
+void studentList::read()
+{
 }
--- CS215-Intro-to-CPP/Project3/Project3/studentList.h
+++ CS215-Intro-to-CPP/Project3/Project3/studentList.h
@@ -1,6 +1,19 @@
 #pragma once
+#include "student.h"
+#include <string>
+
+using namespace std;
+
+const int MAX_STUDENTS = 50;
 class studentList {
 public:
 	studentList();
+	void print();
+	student search(string id); // Searches with student id
+	student search(int idx); // Searches with index
+	void read(); // Reads input file for student data
+
 private:
+	student stuArray[MAX_STUDENTS];
+	int numStudents;
 };
