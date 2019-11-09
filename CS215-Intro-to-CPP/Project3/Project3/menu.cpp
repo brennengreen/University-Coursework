f41d88fabd4ae761867c7cce7003ab34aa0dd918
Brennen Green <brennengreen@outlook.com>
2019-11-09 10:16:31 -0500

    CS215 Proj3 | Menu Class

--- CS215-Intro-to-CPP/Project3/Project3/menu.cpp
+++ CS215-Intro-to-CPP/Project3/Project3/menu.cpp
@@ -1,5 +1,44 @@
 #include "menu.h"
+#include <string>
+
+using namespace std;
 
 menu::menu() {
+	title = string();
+	error = string();
+	for (int i = 0; i < MAX_OPTIONS; i++)
+	{
+		optionDescs[i] = string();
+		menuOptions[i] = char();
+	}
+}
 
+void menu::setTitle(string newTitle)
+{
+	title = newTitle;
+}
+
+void menu::setErrorMsg(string newError)
+{
+	error = newError;
+}
+
+void menu::addOption(char identifier, string desc)
+{
+	if (numOptions < MAX_OPTIONS)
+	{
+		menuOptions[numOptions] = identifier;
+		optionDescs[numOptions] = desc;
+		numOptions++;
+	}
+}
+
+char menu::doMenu()
+{
+	return 0;
+}
+
+bool menu::validOption()
+{
+	return false;
 }
--- CS215-Intro-to-CPP/Project3/Project3/menu.h
+++ CS215-Intro-to-CPP/Project3/Project3/menu.h
@@ -1,6 +1,29 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
+const int MAX_OPTIONS = 10;
+
 class menu {
 public:
 	menu();
+
+	// Mutators
+	void setTitle(string newTitle);
+	void setErrorMsg(string newError);
+	void addOption(char identifier, string desc);
+
+	// Accessors
+
+	// Menu Controls
+	char doMenu();
+	bool validOption();
 private:
+	char menuOptions[MAX_OPTIONS];
+	int numOptions;
+	string optionDescs[MAX_OPTIONS]; // Parallel to menu options
+	string title;
+	string error;
+
 };
